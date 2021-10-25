cc38d342a93687cc5455fb037de7d9b08a43a201
benwade91 <70988204+benn925@users.noreply.github.com>
2021-10-25 07:41:55 -0700

    changed honeybee timer

--- static/app.js
+++ static/app.js
@@ -1,3 +1,4 @@
+// sliding text event loop
 let tl = gsap.timeline({
     scrollTrigger: {
         trigger: '.home',
@@ -6,7 +7,7 @@ let tl = gsap.timeline({
         scrub: 1
     }
 })
-
+// logo event loop
 let tl2 = gsap.timeline({
     scrollTrigger: {
         trigger: '.home',
@@ -15,16 +16,16 @@ let tl2 = gsap.timeline({
         scrub: 1
     }
 })
-
+// honeybee event loop
 let tl4 = gsap.timeline({
     scrollTrigger: {
         trigger: '#about',
-        start: '0%',
-        end: '30%',
+        start: '30%',
+        end: '80%',
         scrub: 1
     }
 })
-
+// home event loop
 let tl3 = gsap.timeline({
     scrollTrigger: {
         trigger: '.home',
@@ -41,6 +42,7 @@ let tl3 = gsap.timeline({
 tl.fromTo(".sliding-text", {y:0}, {y:-200});
 tl4.fromTo(".honeybee", {left: '20%', top: '180vh'}, {left: '100%', top: '110%', opacity: .4})
 
+// controls logo based on windo size
 if(window.innerWidth > 600){
     tl2.fromTo(".logo", {scale: 8}, {scale: 1, top: '6vh', left: '4vw', x: '10%'})
 }else{
