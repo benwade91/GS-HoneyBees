d9e6333347a90c7cb702e7fa2224553e62e9ff47
benwade91 <70988204+benn925@users.noreply.github.com>
2021-10-25 07:52:15 -0700

    changed bee path

--- static/app.js
+++ static/app.js
@@ -40,7 +40,7 @@ let tl3 = gsap.timeline({
 
 
 tl.fromTo(".sliding-text", {y:0}, {y:-200});
-tl4.fromTo(".honeybee", {left: '20%', top: '180vh'}, {left: '100%', top: '110%', opacity: .4})
+tl4.fromTo(".honeybee", {left: '20%', top: '180vh'}, {left: '100%', top: '150%', opacity: .4})
 
 // controls logo based on windo size
 if(window.innerWidth > 600){
